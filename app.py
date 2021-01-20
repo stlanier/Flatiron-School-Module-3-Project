cb9c0303186f0200e340d9c9ba274ccdf26798f4
Stephen T. Lanier <s.lanier.131@gmail.com>
2021-01-20 15:14:54 -0800

    updated legend and improved user interface

--- app.py
+++ app.py
@@ -14,28 +14,6 @@ raw_df = pd.read_csv('data/app_data.csv')
 
 '# Analyze Your Risk of Heart Disease'
 
-if st.sidebar.checkbox('Legend'):  
-    st.sidebar.write('''
-    |Key|Meaning|Interpretation|
-    |---|---|---|
-    |_age_|age|0-99|
-    |_sex_|sex|0 for female, 1 for male|
-    |_cp_|||
-    |_trestbps_|||
-    |_chol_|||
-    |_fbs_|||
-    |_restecg_|||
-    |_thalach_|||
-    |_exang_|||
-    |_oldpeak_|||
-    |_slop_|||
-    |_ca_|||
-    |_thal_|||
-    |_pred_attribute_|||
-    ''')
-if st.checkbox('Raw Data'):
-    raw_df
-
 option1 = st.sidebar.selectbox('Factor 1', (['-']+list(raw_df.columns))[:-1])
 option2 = st.sidebar.selectbox('(Optional) Factor 2', (['-']+list(raw_df.columns))[:-1])
 option3 = st.sidebar.selectbox('(Optional) Factor 3', (['-']+list(raw_df.columns))[:-1])
@@ -71,3 +49,24 @@ if len(options)==3:
     fig.colorbar(sctt, ax = ax, shrink = 0.5, aspect = 15, ticks=[0,1], boundaries = [-.5, .5, 1.5], label='pred_attribute')
     st.pyplot(fig)
     
+if st.checkbox('Legend'):  
+    st.write('''
+    |Key|Meaning|Interpretation|
+    |---|---|---|
+    |_age_|age|0-99|
+    |_sex_|sex|0 for female, 1 for male|
+    |_cp_|chest pain|from 0 to 4 for worst|
+    |_trestbps_|blood pressure on admission to hospital|mm Hg|
+    |_chol_|serum cholesterol|mg/dl|
+    |_fbs_|fasting blood sugar > 120 mg/dl|0 for no, 1 for yes|
+    |_restecg_|resting electrocardiographic results|0-2|
+    |_thalach_|max heart rate achieved|––|
+    |_exang_|exercise induced angina|0 for no, 1 for yes|
+    |_oldpeak_|ST depression induced by exercise relative to rest|––|
+    |_slop_|slope of the peak exercise ST segment|––|
+    |_ca_|number of major vessels colored by fluoroscopy|0-3|
+    |_thal_|––|3 is normal, 6 is fixed defect, 7 is reversable defect|
+    |_pred_attribute_|heart disease diagnosis|0 is no disease; 1 for some degree of disease|
+    ''')
+if st.checkbox('Raw Data'):
+    raw_df
